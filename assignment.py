77f3c3ccee058b6c10976383e3065f5c0fca0ebe
Gloria Kagwiria <nkathapurity024@gmail.com>
2026-03-13 08:23:06 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,47 @@
+Part 2: Python Basics
+
+Question 4
+
+Write a Python program that prints the following message:
+Hello AI Students
+
+Question 5
+
+Create the following variables in Python:
+name
+age
+city
+Print them so the output looks like this:
+
+Name: John
+Age: 20
+City: Nairobi
+
+Question 6
+Create a list of five programming languages.
+
+Example:
+["Python", "Java", "C++", "JavaScript", "Go"]
+
+Print the first language in the list.
+
+Question 7
+Create a dictionary  with the following student information:
+Name
+Age
+Course
+Print the student's name.
+
+Example dictionary format:
+student = {
+"name": "John",
+"age": 21,
+"course": "AI"
+}
+
+Bonus Question (Optional)
+Explain the difference between:
+A list
+A dictionary
+
 
