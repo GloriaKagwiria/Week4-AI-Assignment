8dc82fce08cff54ecce073d5c4ca5b830371bcc4
unknown <nkathapurity024@gmail.com>
2026-03-13 10:03:59 +0300

    complete assignment.py

--- assignment.py
+++ assignment.py
@@ -1,47 +1,81 @@
-Part 2: Python Basics
+"Part 2"
+"Python Basics" 
 
-Question 4
+"Question 4"
 
-Write a Python program that prints the following message:
-Hello AI Students
+"Write a Python program that prints the following message:"
+"Hello AI Students"
 
-Question 5
 
-Create the following variables in Python:
-name
-age
-city
-Print them so the output looks like this:
+print("Hello AI Students")
 
-Name: John
-Age: 20
-City: Nairobi
 
-Question 6
-Create a list of five programming languages.
+"Question 5"
 
-Example:
+"Create the following variables in Python:"
+"name"
+"age"
+"city"
+"Print them so the output looks like this:"
+
+"Name: John"
+"Age: 20"
+"City: Nairobi"
+
+
+
+"Name=John"
+"Age=20"
+"City=Nairobi"
+print("Name:John")
+print("Age:20")
+print("City:Nairobi")
+
+"Question 6"
+"Create a list of five programming languages."
+
+"Example:"
 ["Python", "Java", "C++", "JavaScript", "Go"]
 
-Print the first language in the list.
+"Print the first language in the list."
 
-Question 7
-Create a dictionary  with the following student information:
-Name
-Age
-Course
-Print the student's name.
 
-Example dictionary format:
+
+Programming_Languages=["Python","Java","C++","JavaScript","Go"]
+print(Programming_Languages[0])
+
+
+"Question 7"
+"Create a dictionary  with the following student information:"
+"Name"
+"Age"
+"Course"
+"Print the student's name."
+
+"Example dictionary format:"
 student = {
 "name": "John",
 "age": 21,
 "course": "AI"
 }
 
-Bonus Question (Optional)
-Explain the difference between:
-A list
-A dictionary
+
+student_information = {
+    "Name":"John",
+    "Age":21,
+    "Course":"AI"
+    }
+print(student_information["Name"])
+
+
+
+"Bonus Question (Optional)"
+"Explain the difference between:"
+"A list"
+"A dictionary"
+
+
+print("A Dictionary is like an object and stores key-value data")
+print("A List is an object inside a variable that stores multiple data")
 
 
